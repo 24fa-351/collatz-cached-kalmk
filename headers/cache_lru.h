10d1a555cf1312122362a931cbbe8c50813f3d92
kalmk <111930167+kalmk@users.noreply.github.com>
2024-09-28 23:38:57 -0700

    professional

--- headers/cache_lru.h
+++ headers/cache_lru.h
@@ -1,3 +1,38 @@
+// Copyright (C) 2024 Ju Yeong Kim
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License version 3 as published by the
+// Free Software Foundation. This program is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details. A copy of the GNU General Public License v3
+// is available here:  <https://www.gnu.org/licenses/>.
+//
+//=================================================================================================
+// Name:
+//     Ju Yeong Kim
+//
+//
+// Course ID:
+//     CPSC 351
+//
+//
+// Name of the program:
+//     Collatz-Cached
+//
+//
+// OS of the computer where the program was developed:
+//     Ubuntu 22.04 WSL
+//
+//
+// OS of the computer where the program was tested:
+//     Ubuntu 22.04 WSL
+//=================================================================================================
+//
+//
+//
+//=======Begin code area
+
 #ifndef CACHE_LRU_H
 #define CACHE_LRU_H
 
